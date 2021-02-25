4cc7fc61f5f4bc46a4ab010446603213a54daf74
ilya <ilya129129@gmail.com>
2021-02-25 14:00:46 +0300

    initial commit

---
+++ post.rb
@@ -0,0 +1,36 @@
+class Post
+  def self.post_types
+    [Memo, Task, Link]
+  end
+
+  def self.create(type_index)
+    post_types[type_index].new
+  end
+
+  def initialize
+    @created_at = Time.now
+    @text = []
+  end
+
+  def read_from_console
+  end
+
+  def to_strings
+  end
+
+  def save
+    file = File.new(file_path, 'w:UTF-8')
+
+    to_strings.each { |string| file.puts(string) }
+
+    file.close
+  end
+
+  def file_path
+    current_path = File.dirname(__FILE__)
+
+    file_time = @created_at.strftime('%Y-%m-%d_%H-%M-%S')
+
+    "#{current_path}/#{self.class.name}_#{file_time}.txt"
+  end
+end
